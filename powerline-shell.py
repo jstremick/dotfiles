5da1fc28ee86b56b68a1d8e9be7cd760ad6cc3dc
James Stremick <james.stremick@vocalocity.com>
2013-07-12 11:14:01 -0400

    snipmate bundle, tmux config, powerline update

--- powerline-shell.py
+++ powerline-shell.py
@@ -283,21 +283,6 @@ except subprocess.CalledProcessError:
     pass
 
 
-import os
-import re
-import subprocess
-
-def add_jobs_segment():
-    ppid = os.getppid()
-    output = subprocess.Popen(['ps', '-a', '-o', 'ppid'], stdout=subprocess.PIPE).communicate()[0]
-    num_jobs = len(re.findall(str(ppid), output)) - 1
-
-    if num_jobs > 0:
-        powerline.append(' %d ' % num_jobs, Color.JOBS_FG, Color.JOBS_BG)
-
-add_jobs_segment()
-
-
 def add_root_indicator_segment():
     root_indicators = {
         'bash': ' \\$ ',
