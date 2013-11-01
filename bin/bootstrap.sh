72abe44299f3e1acac09ed12845f71157e74e48b
James Stremick <james.stremick@vocalocity.com>
2013-10-31 22:07:21 -0400

    use maximum-awesome as my base

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -2,21 +2,31 @@
 
 set -e
 
-echo "prerequisites: python git pip dulwich tmux weechat offlineimap mutt hg ack zsh vim"
-
 function ensure_link {
     test -L "$HOME/$2" || ln -fs "$HOME/$1" "$HOME/$2"
   }
 
 
-  ensure_link "dotfiles/ackrc"               ".ackrc"
-  ensure_link "dotfiles/gemrc"               ".gemrc"
-  ensure_link "dotfiles/gitconfig"           ".gitconfig"
-  ensure_link "dotfiles/gitignore"           ".gitignore"
-  ensure_link "dotfiles/gitignore_global"    ".gitignore_global"
-  ensure_link "dotfiles/powerline-shell.py"  "powerline-shell.py"
-  ensure_link "dotfiles/tmux.conf"           ".tmux.conf"
-  ensure_link "dotfiles/vim"                 ".vim"
-  ensure_link "dotfiles/vim/vimrc"           ".vimrc"
+ensure_link "dotfiles/ackrc"               ".ackrc"
+ensure_link "dotfiles/gemrc"               ".gemrc"
+ensure_link "dotfiles/gitconfig"           ".gitconfig"
+ensure_link "dotfiles/gitignore"           ".gitignore"
+ensure_link "dotfiles/gitignore_global"    ".gitignore_global"
 
-  echo completed
+if [ $(uname) = 'Darwin'  ] ; then
+
+  git clone https://github.com/square/maximum-awesome.git $HOME/maximum-awesome
+  cd $HOME/maximum-awesome && rake
+  ensure_link "dotfiles/vim/vimrc.bundle.local" ".vimrc.bundle.local"
+  ensure_link "dotfiles/vim/vimrc.local"        ".vimrc.local"
+  ensure_link "dotfiles/powerline-shell.py"     "powerline-shell.py"
+
+else
+  git clone https://github.com/timthrillist/minimum-awesome.git $HOME/minimum-awesome
+  cd $HOME/minimum-awesome
+  chmod a+x install.sh && ./install.sh
+
+fi
+
+
+echo completed.
